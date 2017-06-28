e8a2d5d2159158347c250b4619f9765797aaac93
seba3c <castanedacs@gmail.com>
2017-06-27 23:02:34 -0300

    Initial django project layout

---
+++ todo_list_server/settings/dev.py
@@ -0,0 +1,62 @@
+import os
+
+from todo_list_server.settings.base import *
+
+DEBUG = True
+
+LOGGING_DEFAULT_LEVEL = 'DEBUG'
+LOGGING_CONSOLE_HANDLER = 'console'
+LOGGING_FILE_HANDLER = 'file_default'
+LOGGING_DEFAULT_FILENAME = os.path.join(LOGS_DIR, 'todolist.log')
+LOGGING_DEFAULT_HANDLERS = [LOGGING_CONSOLE_HANDLER, LOGGING_FILE_HANDLER]
+
+# Logging config
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)s %(levelname)s] (%(module)s): %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        LOGGING_CONSOLE_HANDLER: {
+            'level': LOGGING_DEFAULT_LEVEL,
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+            'filters': ['require_debug_true'],
+        },
+        LOGGING_FILE_HANDLER: {
+            'level': LOGGING_DEFAULT_LEVEL,
+            'class': 'logging.FileHandler',
+            'filename': LOGGING_DEFAULT_FILENAME,
+            'formatter': 'verbose',
+            'filters': [],
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': LOGGING_DEFAULT_HANDLERS,
+            'level': LOGGING_DEFAULT_LEVEL,
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': LOGGING_DEFAULT_HANDLERS,
+            'level': LOGGING_DEFAULT_LEVEL,
+            'propagate': False,
+        },
+        'todolist': {
+            'handlers': LOGGING_DEFAULT_HANDLERS,
+            'level': LOGGING_DEFAULT_LEVEL,
+            'propagate': False,
+        },
+    }
+}
