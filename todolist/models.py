717d8fbbbdeb47d13b229402ae748c819d2cca3d
seba3c <castanedacs@gmail.com>
2017-06-29 20:12:43 -0300

    Added initial models

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
+import logging
+
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import ugettext as _
+
+
+logger = logging.getLogger(__name__)
+
+
+class TodoList(models.Model):
+
+    created_at = models.DateTimeField(_("Created at"), null=False, blank=False, auto_now_add=True)
+    title = models.CharField(_("Title"), max_length=50, null=False, blank=False)
+
+    class Meta:
+        verbose_name = "TODO-LIST"
+        verbose_name_plural = "TODO-LISTs"
+
+
+class TodoListItem(models.Model):
+
+    desc = models.CharField(_("Description"), max_length=50, null=False, blank=False)
+    done = models.BooleanField(_("Done"), default=False)
+    todo_list = models.ForeignKey(TodoList, on_delete=models.CASCADE, related_name="items")
+
+    class Meta:
+        verbose_name = "TODO-LIST Item"
+        verbose_name_plural = "TODO-LIST Items"
